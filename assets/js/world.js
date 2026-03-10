ce720be30fe46d7d0ec010e41b7c802060c5e317
Finn <laskowski.finn@schueler.6072-fuerth.de>
2026-03-10 15:00:14 +0100

    Limit tree spawning to a maximum of 2 trees

--- assets/js/world.js
+++ assets/js/world.js
@@ -68,7 +68,7 @@ export function spawnTrees() {
         }
     }
     
-    if (currentTreeCount < 5) {
+    if (currentTreeCount < 2) {
         let spawned = false; 
         let attempts = 0;
         
